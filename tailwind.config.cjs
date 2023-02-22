c31ab7ca70a64ccef6658465323de2609fc0e751
Toni <twtiainen@gmail.com>
2023-02-22 13:16:48 +0200

    Add dependencies and configs

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Inter']
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: 0 },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+        },
+    },
+    plugins: [require("tailwindcss-animate")],
+    darkMode: 'class'
+}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { plugin as mdPlugin } from 'vite-plugin-markdown';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+        tsconfigPaths(),
+        mdPlugin({ mode: ['html', 'toc'] }),
+        sveltekit()
+    ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
