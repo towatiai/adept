19e193ca6cc674b48f9233d49416df8d739e9b34
Toni <twtiainen@gmail.com>
2023-02-22 23:39:05 +0200

    Add sliding animation to accordion

--- src/components/ui/accordion/AccordionContent.svelte
+++ src/components/ui/accordion/AccordionContent.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { cn } from "$lib/utils";
     import { Accordion } from "teil-ui";
+    import { slide } from "svelte/transition";
 </script>
 
 <Accordion.Content {...$$props} class={cn(
     "overflow-hidden text-sm transition-all data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up",
     $$props.class
 )}>
-    <div class="pt-0 pb-4"><slot/></div>
+    <div class="pt-0 pb-4" transition:slide><slot/></div>
 </Accordion.Content>
