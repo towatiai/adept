17187f6d5513475a1442282f9c0e64c95b1e38b7
Toni <twtiainen@gmail.com>
2023-02-27 22:52:54 +0200

    Add forms to tabs example

--- src/examples/components/Tabs.svelte
+++ src/examples/components/Tabs.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+    import Button from "@/components/ui/Button.svelte";
+    import Input from "@/components/ui/Input.svelte";
+    import Label from "@/components/ui/Label.svelte";
     import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 </script>
 
@@ -11,10 +14,34 @@
 
     <TabsContent value="account">
         <p class="text-sm text-slate-500">Make changes to your account here. Click save when you're done.</p>
+        <div class="grid gap-2 py-4">
+            <div class="space-y-1">
+                <Label>Name</Label>
+                <Input value="Pedro Duarte"/>
+            </div>
+            <div class="space-y-1">
+                <Label>Username</Label>
+                <Input value="@peduarte"/>
+            </div>
+        </div>
+        
+        <Button>Save changes</Button>
     </TabsContent>
 
     <TabsContent value="password">
-        <p>password</p>
+        <p class="text-sm text-slate-500">Change your password here. After saving, you'll be logged out.</p>
+        <div class="grid gap-2 py-4">
+            <div class="space-y-1">
+                <Label>Current password</Label>
+                <Input type="password"/>
+            </div>
+            <div class="space-y-1">
+                <Label>New password</Label>
+                <Input type="password"/>
+            </div>
+        </div>
+        
+        <Button>Save password</Button>
     </TabsContent>
 
 </Tabs>
